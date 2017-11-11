3c06bee49618f59fe645003849cf6e0fbe99d968
andreea penu <andreea_penu@hotmail.com>
2017-11-11 14:52:40 +0100

    React crash course - begin

---
+++ nieuwe-technologie/code/myapp/app/components/Component1/Component1.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import {AppRegistry, Text, View} from 'react-native';
+
+
+export default class Component1 extends Component{
+    render(){
+        return(
+            <View>
+                <Text>This is Component1</Text>
+            </View>
+        );
+    }
+}
+
+AppRegistry.registerComponent('Component1', () => Component1);
